02d39833e66ecd0c8bc2dfefb3b18b88246645c8
RFR-R1CH4RD <127535312+RFR-R1CH4RD@users.noreply.github.com>
2023-03-16 22:28:00 +0100

    Update DevilHub.lua

--- DevilHub.lua
+++ DevilHub.lua
@@ -442,7 +442,7 @@ function Library:create(options)
 
 	self.CurrentTheme = options.Theme
 
-	local gui = self:object"DevilHub", {
+	local gui = self:object"ScreenGui", {
 		Parent = (RunService:IsStudio() and LocalPlayer.PlayerGui) or game:GetService("CoreGui"),
 		ZIndexBehavior = Enum.ZIndexBehavior.Global
 	})
