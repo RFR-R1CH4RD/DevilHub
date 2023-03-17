80a620bd02276a2743d7eaf1967e38d8d03ee8b7
RFR-R1CH4RD <127535312+RFR-R1CH4RD@users.noreply.github.com>
2023-03-17 23:20:56 +0100

    Update DevilHubUILibrary.lua

--- DevilHubUILibrary.lua
+++ DevilHubUILibrary.lua
@@ -25,13 +25,13 @@ local Library = {
 			StrongText = Color3.fromHSV(0, 0, 1),		
 			WeakText = Color3.fromHSV(0, 0, 172/255)
 		},
-		Serika = {
-			Main = Color3.fromRGB(50, 52, 55),
-			Secondary = Color3.fromRGB(80, 82, 85),
-			Tertiary = Color3.fromRGB(226, 183, 20),
+		Celestial = {
+			Main = Color3.fromRGB(255, 255, 255),
+			Secondary = Color3.fromRGB(212, 185, 41),
+			Tertiary = Color3.fromRGB(174, 159, 79),
 
-			StrongText = Color3.fromHSV(0, 0, 1),		
-			WeakText = Color3.fromHSV(0, 0, 172/255)
+			StrongText = Color3.fromHSV(0, 0, 0),		
+			WeakText = Color3.fromHSV(0, 0, 0)
 		},
 		Diabolic = {
 			Main = Color3.fromRGB(195, 12, 12),
