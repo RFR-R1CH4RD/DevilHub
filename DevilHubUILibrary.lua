48a6630413441e762ab90e61232805182117742d
RFR-R1CH4RD <127535312+RFR-R1CH4RD@users.noreply.github.com>
2023-03-18 15:39:40 +0100

    Update DevilHubUILibrary.lua

--- DevilHubUILibrary.lua
+++ DevilHubUILibrary.lua
@@ -36,7 +36,7 @@ local Library = {
 		Diabolic = {
 			Main = Color3.fromRGB(195, 12, 12),
 			Secondary = Color3.fromRGB(99, 0, 0),
-			Tertiary = Color3.fromRGB(180, 26, 26),
+			Tertiary = Color3.fromRGB(225, 76, 76),
 
 			StrongText = Color3.fromHSV(0, 0, 100),		
 			WeakText = Color3.fromHSV(0, 0, 100)
