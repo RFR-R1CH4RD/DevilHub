6eb4b52c8fc483c59a359e8708c5d9e9bbcff05b
RFR-R1CH4RD <127535312+RFR-R1CH4RD@users.noreply.github.com>
2023-03-19 13:21:48 +0100

    Update DevilHubUILibrary.lua

--- DevilHubUILibrary.lua
+++ DevilHubUILibrary.lua
@@ -1768,7 +1768,7 @@ function Library:button(options)
 
 	local buttonContainer = self.container:object("TextButton", {
 		Theme = {BackgroundColor3 = "Secondary"},
-		Size = UDim2.new(1, -20, 0, 52)
+		Size = UDim2.new(1, -14, 0, 52)
 	}):round(7)
 
 	local text = buttonContainer:object("TextLabel", {
