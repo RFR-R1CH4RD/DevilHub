14f89bf5e7a0e213d57996d7da9a9d5db569caaa
RFR-R1CH4RD <127535312+RFR-R1CH4RD@users.noreply.github.com>
2023-04-26 22:10:02 +0200

    Update DevilHubUILibrary.lua

--- DevilHubUILibrary.lua
+++ DevilHubUILibrary.lua
@@ -644,7 +644,7 @@ function Library:create(options)
 		Position = UDim2.new(0, 5, 1, -6),
 		Size = UDim2.new(0.2, 0, 0, 10),
 		Font = Enum.Font.SourceSans,
-		Text = "Version | v0.45",
+		Text = "Version | v0.46",
 		Theme = {TextColor3 = "Tertiary"},
 		TextSize = 14,
 		TextXAlignment = Enum.TextXAlignment.Left
